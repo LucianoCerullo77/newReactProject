8b8646e458e88998f59c205762f419c563bc8d4d
Luciano Cerullo <lucianonicolas10@gmail.com>
2022-07-01 14:43:48 -0300

    made an navbar from boostrap

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-
+import Header from "./components/Header/Header";
 
 function App() {
   return (
     <div className="App">
+      <Header/>
       <h1>React New Project Example</h1>
     </div>
   );
