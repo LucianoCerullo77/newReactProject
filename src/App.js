148d2034707f427e73de5ad125112ea7bbb7e8de
Luciano Cerullo <lucianonicolas10@gmail.com>
2022-07-20 12:57:22 -0300

    singIn and Chat components creation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './styles/App.css'
+import SingIn from './components/SingIn/SingIn';
+import Chat from './components/Chat/Chat';
 
 function App() {
   return (
     <>
-
+    <SingIn/>
+    <Chat/>
     </>
   );
 }
