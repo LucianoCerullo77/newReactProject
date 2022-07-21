2161e98b67af06d8abb55123ac6e2d0e418fcfcd
Luciano Cerullo <lucianonicolas10@gmail.com>
2022-07-21 13:24:37 -0300

    Chat Fixed, Sending Messages won´t work by now

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -1,27 +1,31 @@
-import React, {useState, useEffect}  from 'react'
-import { db } from '../../firebase';
+import React, {useState,useEffect} from 'react'
+import {db} from '../../firebase'
 import LogOut from '../LogOut/LogOut'
-import SendMessage from '../SendMessage/SendMessage';
+import SendMessage from '../SendMessage/SendMessage'
 
 function Chat() {
   const [messages, setMessages] = useState([])
-  useEffect(() => {
-    db.collection('messages').orderBy('createdAt').limit(30).onSnapshot(snapshot => {
-      setMessages(snapshot.docs.map(doc => doc.data()))
-    })
-  },[]);
+  useEffect(()=> {
+    db.collection('messages')
+    .orderBy('createdAt')
+    .limit(15)
+    .onSnapshot(snapshot => {
+      setMessages(snapshot.docs.map(doc => ({id: doc.id, message: doc.data()})))
+    }
+    )
+  }, [])
   return (
     <div>
+      <h1>Chat</h1>
       <LogOut/>
-      {messages.map(({ id, text, photoURL }) => {
-        <div key={id}>
-          <img src={photoURL} alt="userPhoto" />
-          <p>{text}</p>
-        </div>;
-      })}
-      <SendMessage/>
-    </div>
-  )
-}
+      {messages.map(({id, message, photoURl}) => (
+      <div key={id}>
+        <img src={photoURl} alt="userPhoto"/>
+        <p>{message.text}</p>
+      </div>
+    ))}
+    <SendMessage/>
+  </div>
+  )} 
 
 export default Chat
--- src/components/SendMessage/sendMessage.js
+++ src/components/SendMessage/sendMessage.js
@@ -3,28 +3,19 @@ import {db, auth} from '../../firebase'
 import firebase from 'firebase/compat/app'
 import 'firebase/compat/auth'
 import 'firebase/compat/firestore'
-import { Button, Form, } from 'react-bootstrap'
+import { Button, Form, InputGroup, } from 'react-bootstrap'
 
 function SendMessage() {
     const [message, setMessage] = useState('')
-    async function sendMessage(e) {
-        e.preventDefault()
-        const {uid, photoURL} = auth.currentUser
-        
-        await db.collection('messages').add({
-            text: message,
-            photoURL,
-            uid,
-            createdAt: new firebase.firestore.FieldValue.serverTimestamp()
-        })
-        setMessage('')
-    }
+    
 
   return (
     <div>
-        <Form onSubmit={sendMessage}>
-            <Form.Text value={message} onChange={(e)=> setMessage.target.value} type="text" placeholder="Type a message..."/>
-            <Button type="submit">Send</Button>
+        <Form>
+        <InputGroup className="mb-auto">
+        <Form.Control type="text" value={message} onChange={(e)=> setMessage(e.target.value)} placeholder="Type a message..." />
+        <Button>Send</Button>
+        </InputGroup>
         </Form>
     </div>
   )
