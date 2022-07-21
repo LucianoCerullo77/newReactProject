b775201b6da9b3fd523795969fa762b39e5b4e2b
Luciano Cerullo <lucianonicolas10@gmail.com>
2022-07-21 13:48:05 -0300

    Sending messages works but no message apears in chat. It appears in Firebase

--- src/components/SendMessage/sendMessage.js
+++ src/components/SendMessage/sendMessage.js
@@ -8,14 +8,25 @@ import { Button, Form, InputGroup, } from 'react-bootstrap'
 function SendMessage() {
     const [message, setMessage] = useState('')
     
+    async function sendMessage(e) {
+        e.preventDefault()
+        await db.collection('messages').add({
+            message: message,
+            user: auth.currentUser.displayName,
+            photoURL: auth.currentUser.photoURL,
+            userId: auth.currentUser.uid,
+            createdAt: firebase.firestore.FieldValue.serverTimestamp()
+        })
+        setMessage('')
+    }
 
   return (
     <div>
-        <Form>
+        <Form onSubmit={sendMessage}>
         <InputGroup className="mb-auto">
         <Form.Control type="text" value={message} onChange={(e)=> setMessage(e.target.value)} placeholder="Type a message..." />
-        <Button>Send</Button>
         </InputGroup>
+        <Button type="submit">Send</Button>
         </Form>
     </div>
   )
